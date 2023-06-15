9a704aaed526dd1b6f8be87b170a31fcfb4f7220
jahearn3 <josephahearn3@gmail.com>
2023-06-15 09:19:37 -0700

    included Bellevue trips

--- main.py
+++ main.py
@@ -14,3 +14,8 @@ mp.duration_vs_departure(filename, df, start='work', end='home', gbr=False, dtr=
 filename = 'commuting_port_orchard_ferry.csv'
 df = dp.process_data(filename)
 mp.duration_vs_departure(filename, df, gbr=False, dtr=False, rfr=False, nn=False, xgb=False, ensemble_r=False)
+
+filename = 'commuting_bellevue.csv'
+df = dp.process_data(filename)
+mp.duration_vs_departure(filename, df, gbr=False, dtr=False, rfr=False, nn=False, xgb=False, ensemble_r=False)
+mp.duration_vs_departure(filename, df, start='work', end='home', gbr=False, dtr=False, rfr=False, nn=False, xgb=False, ensemble_r=False)
