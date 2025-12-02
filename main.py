35f1bb5255bcf1f417a235f37b6296c768b3b84d
jahearn3 <josephahearn3@gmail.com>
2025-12-02 11:32:19 -0800

    Restructured main into a loop with conditions and variables

--- main.py
+++ main.py
@@ -1,45 +1,25 @@
 import data_processing as dp
 import make_plots as mp
 
-# filename = 'commuting_tenino.csv'
-# df = dp.process_data(filename)
-# mp.duration_vs_departure(filename, df,
-#                          gbr=True, dtr=True, rfr=True, nn=False,
-#                          xgb=True, ensemble_r=True)
-# mp.duration_vs_departure(filename, df, start='work', end='home',
-#                          gbr=True, dtr=True, rfr=True, nn=False,
-#                          xgb=True, ensemble_r=True)
+trips = ['tenino', 'port_orchard_driving', 'port_orchard_ferry', 'bellevue']
+for trip in trips:
+    if trip in ['tenino', 'port_orchard_ferry', 'bellevue']:
+        continue
 
-filename = 'commuting_port_orchard_driving.csv'
-df = dp.process_data(filename)
-# mp.duration_vs_departure(filename, df,
-#                          gbr=False, dtr=False, rfr=False, nn=False,
-#                          xgb=False, ensemble_r=False)
-# mp.duration_vs_departure(filename, df, start='work', end='home',
-#                          gbr=False, dtr=False, rfr=False, nn=False,
-#                          xgb=False, ensemble_r=False)
-mp.duration_vs_departure(filename, df,
-                         gbr=True, dtr=True, rfr=True, nn=False,
-                         xgb=True, ensemble_r=True)
-mp.duration_vs_departure(filename, df, start='work', end='home',
-                         gbr=True, dtr=True, rfr=True, nn=False,
-                         xgb=True, ensemble_r=True)
+    gbr = True if trip != 'bellevue' else False
+    dtr = True if trip != 'bellevue' else False
+    rfr = True if trip != 'bellevue' else False
+    nn = False
+    xgb = True if trip != 'bellevue' else False
+    ensemble_r = True if trip != 'bellevue' else False
 
-# filename = 'commuting_port_orchard_ferry.csv'
-# df = dp.process_data(filename)
-# mp.duration_vs_departure(filename, df,
-#                          gbr=False, dtr=False, rfr=False, nn=False,
-#                          xgb=False, ensemble_r=False)
-# mp.duration_vs_departure(filename, df,
-#                          gbr=True, dtr=True, rfr=True, nn=False,
-#                          xgb=True, ensemble_r=True)
-# mp.driving_and_waiting_vs_departure(filename, df)
-
-filename = 'commuting_bellevue.csv'
-df = dp.process_data(filename)
-# mp.duration_vs_departure(filename, df,
-#                          gbr=False, dtr=False, rfr=False, nn=False,
-#                          xgb=False, ensemble_r=False)
-mp.duration_vs_departure(filename, df, start='work', end='home',
-                         gbr=False, dtr=False, rfr=False, nn=False,
-                         xgb=False, ensemble_r=False)
+    filename = f'commuting_{trip}.csv'
+    df = dp.process_data(filename)
+    mp.duration_vs_departure(filename, df,
+                             gbr=gbr, dtr=dtr, rfr=rfr, nn=nn,
+                             xgb=xgb, ensemble_r=ensemble_r)
+    mp.duration_vs_departure(filename, df, start='work', end='home',
+                             gbr=gbr, dtr=dtr, rfr=rfr, nn=nn,
+                             xgb=xgb, ensemble_r=ensemble_r)
+    if trip == 'port_orchard_ferry':
+        mp.driving_and_waiting_vs_departure(filename, df)
